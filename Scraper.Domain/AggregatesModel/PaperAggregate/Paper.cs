f39b2c5b5d0863aad4588d03d6c805546c72ce12
0surface <tms.0xvoid@gmail.com>
2020-05-12 11:36:23 +0100

    Add project files.

---
+++ Scraper.Domain/AggregatesModel/PaperAggregate/Paper.cs
@@ -0,0 +1,11 @@
+﻿using Scraper.Domain.SeedWork;
+
+namespace Scraper.Domain.AggregatesModel.PaperAggregate
+{
+    public class Paper
+        : Entity
+    {
+        public string IdString { get; private set; }
+        public string Link { get; private set; }
+    }
+}
