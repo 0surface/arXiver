dc365bb5b85dbc9f0624674765f7db934c4e2e55
0surface <tms.0xvoid@gmail.com>
2020-05-12 12:51:52 +0100

    Add 'Subject' aggregate

---
+++ Scraper.Domain/AggregatesModel/SubjectAggregate/Subject.cs
@@ -0,0 +1,17 @@
+﻿using Scraper.Domain.SeedWork;
+using System.Collections.Generic;
+
+namespace Scraper.Domain.AggregatesModel.SubjectAggregate
+{
+    public class Subject
+        : Entity
+    {
+        public string ArxivId { get; private set; }
+        public string FullName { get; private set; }
+        public string Discipline { get; private set; }
+        public string DisciplineId { get; private set; }
+
+        private List<string> _subCatagories;
+        public IReadOnlyCollection<string> SubCatagories => _subCatagories;
+    }
+}
