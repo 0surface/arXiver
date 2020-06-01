3ff87d95511a58eba5da69a0c5f7d1c8e4d484ca
0surface <tms.0xvoid@gmail.com>
2020-06-01 15:12:05 +0100

    Added contextdb, migrations (trail), visualization tools to API project

---
+++ Scraper.API/Infrastructure/ArticleContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Scraper.Domain.AggregatesModel.ArticleAggregate;
+using Scraper.Domain.AggregatesModel.SubjectAggregate;
+using System.IO;
+
+namespace Scraper.API.Infrastructure
+{
+    public class ArticleContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Version> Versions { get; set; }
+        public DbSet<SubjectItem> SubjectItems { get; set; }        
+        public DbSet<AuthorArticle> AuthorArticles { get; set; }
+
+        public DbSet<Subject> Subjects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var connectionString = configuration["ConnectionString"];
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            /* This helps EF core understand the intended database schema, 
+             * setting up a Many-To-Many relationship between Article and author */
+            modelBuilder.Entity<AuthorArticle>().HasKey(s => new { s.AuthorId, s.ArticleId });
+            modelBuilder.Entity<Discipline>().ToTable("Disciplines");
+            modelBuilder.Entity<ScientificField>().ToTable("ScientificFields");
+        }
+
+    }
+}
+
---
+++ Scraper.API/Migrations/20200601133656_newrelations2.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Scraper.API.Migrations
+{
+    public partial class newrelations2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_Subjects_PrimarySubjectId",
+                table: "Articles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subjects_Articles_ArticleId",
+                table: "Subjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subjects_ArticleId",
+                table: "Subjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_PrimarySubjectId",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "ArticleId",
+                table: "Subjects");
+
+            migrationBuilder.DropColumn(
+                name: "PrimarySubjectId",
+                table: "Articles");
+
+            migrationBuilder.CreateTable(
+                name: "SubjectItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    IsPrimary = table.Column<bool>(nullable: false),
+                    ArticleId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubjectItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubjectItem_Articles_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "Articles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubjectItem_ArticleId",
+                table: "SubjectItem",
+                column: "ArticleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SubjectItem");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ArticleId",
+                table: "Subjects",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PrimarySubjectId",
+                table: "Articles",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_ArticleId",
+                table: "Subjects",
+                column: "ArticleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_PrimarySubjectId",
+                table: "Articles",
+                column: "PrimarySubjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_Subjects_PrimarySubjectId",
+                table: "Articles",
+                column: "PrimarySubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subjects_Articles_ArticleId",
+                table: "Subjects",
+                column: "ArticleId",
+                principalTable: "Articles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
